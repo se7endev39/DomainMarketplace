8e4e21851b7d711725e239f32538c05e454f0dee
daniel <danielyng112@gmail.com>
2021-05-06 20:17:06 +0900

    starting to add images

---
+++ pages/Mobile/imageCollection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './index.module.css';
+import {MDBCardImage} from 'mdbreact'
+
+function ImageCollection(props) {
+    return (
+        <div className="flex flex-grow 2xl:p-0 pb-20 lg:flex-row flex-col">
+            
+        </div>
+    );
+}
+
+export default ImageCollection;
